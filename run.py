0752163f6d3b99a7d5dde5e8f08aca6779d7eb4a
ii7mood <ii7moodae@gmail.com>
2023-06-19 18:59:56 +0400

    Added option to specify python executable

--- run.py
+++ run.py
@@ -1,12 +1,16 @@
 import subprocess
 from time import sleep
+import common
+
+config = common.config
+python = config['GENERAL']['python_exec']
 
 # Define the scripts you want to execute
 script_paths = ['Discord BOT/app.py', 'Discord BOT/BOT_commands.py', 'Pushover/app.py', 'Detector.py']
 
 # Execute each script
 for script_path in script_paths:
-    subprocess.Popen(['python', script_path], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.Popen([python, script_path], stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
     sleep(3)
 
 # Keep the program running until CTRL + C or something
